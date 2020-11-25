d6ace3920d1c65351cca6180ef7c52a2bafe98fb
shimiaomiao <bj_haomiao@163.com>
2020-11-25 13:50:38 +0800

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'php-amqplib/php-amqplib' => 
+    array (
+      'pretty_version' => 'v2.9.2',
+      'version' => '2.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '76faddcd668dabb8d4f7c00e86b8a9decd781a59',
+    ),
+    'videlalvaro/php-amqplib' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v2.9.2',
+      ),
+    ),
+  ),
+);
